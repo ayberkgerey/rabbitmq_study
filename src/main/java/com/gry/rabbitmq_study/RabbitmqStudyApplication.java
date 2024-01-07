0604491a1305bd1bef17e4db333a3c0cc4821107
Ayberk Gerey <ayberkgerey@gmail.com>
2024-01-07 23:49:12 +0300

    feature: configuration

--- src/main/java/com/gry/rabbitmq_study/RabbitmqStudyApplication.java
+++ src/main/java/com/gry/rabbitmq_study/RabbitmqStudyApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RabbitmqStudyApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RabbitmqStudyApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitmqStudyApplication.class, args);
+    }
 
 }
